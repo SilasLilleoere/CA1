dacbd086505c2c9c5b35a32fcd20dc98007bf312
Hjemme <Hjemme@10.50.138.162>
2015-09-09 13:50:15 +0200

    Added ProtocolStrings in a shared package.

---
+++ src/Shared/Protocols.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Shared;
+
+/**
+ *
+ * @author Hjemme
+ */
+public class Protocols {
+
+    //Client Commands
+    public static final String STOP = "STOP#";
+    public static final String USERNAME = "USER#";
+    public static final String MSG = "MGS#"; //Also used for Server
+
+    //-------------------------------------------
+    //Server Commands
+    public static final String UserList = "USERLIST#";
+
+}
